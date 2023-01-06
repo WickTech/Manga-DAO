213bbdf8720d36efdd07703deb7e8c052de301c8
Rishabh Verma <44228839+WickTech@users.noreply.github.com>
2023-01-06 16:21:36 +0530

    Add files via upload

---
+++ scripts/3-config-nft.js
@@ -0,0 +1,19 @@
+import sdk from "./1-initialize-sdk.js";
+import { readFileSync } from "fs";
+
+
+(async () => {
+  try {
+    const editionDrop = sdk.getEditionDrop("0x88d2f209236C1645c836E842BEE4486d82E55181", "edition-drop");
+    await editionDrop.createBatch([
+      {
+        name: "Immeasurable Void",
+        description: "This NFT will give you access to MangaDAO! & the power to binge read manga",
+        image: readFileSync("scripts/assets/void.jpg"),
+      },
+    ]);
+    console.log("✅ Successfully created a new NFT in the drop!");
+  } catch (error) {
+    console.error("failed to create the new NFT", error);
+  }
+})();

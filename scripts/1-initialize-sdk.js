4b75a07f5e9e77be154a6ea361e112c6acd7a9d2
Rishabh Verma <44228839+WickTech@users.noreply.github.com>
2023-01-10 02:47:09 +0530

    Update 1-initialize-sdk.js

--- scripts/1-initialize-sdk.js
+++ scripts/1-initialize-sdk.js
@@ -1,5 +1,4 @@
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
-import ethers from "ethers";
 
 // Importing and configuring our .env file that we use to securely store our environment variables
 import dotenv from "dotenv";

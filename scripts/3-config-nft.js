943d8bceedc5d2a239e877ef0968f461c119fee8
Rishabh Verma <44228839+WickTech@users.noreply.github.com>
2023-01-10 02:49:47 +0530

    Update 3-config-nft.js

--- scripts/3-config-nft.js
+++ scripts/3-config-nft.js
@@ -1,19 +1,22 @@
 import sdk from "./1-initialize-sdk.js";
 import { readFileSync } from "fs";
 
-const editionDrop = sdk.getEditionDrop("0x88d2f209236C1645c836E842BEE4486d82E55181");
-
 (async () => {
   try {
+    const editionDrop = await sdk.getContract(
+      "0xCDaDf6EB76eb877bC681f29e964F585125024D66",
+      "edition-drop"
+    );
     await editionDrop.createBatch([
       {
         name: "Immeasurable Void",
-        description: "This NFT will give you access to MangaDAO! & the power to binge read manga",
-        image: readFileSync("scripts/assets/void.jpg"),
+        description: "This NFT will give you access to MangaDAO!",
+        image: readFileSync("scripts/assets/void.png"),
       },
     ]);
     console.log("✅ Successfully created a new NFT in the drop!");
-  } catch (error) {
+  } 
+  catch (error) {
     console.error("failed to create the new NFT", error);
   }
 })();
